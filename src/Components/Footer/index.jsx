63e8b9a9f643b36c19f42354763d0227c90c2b39
Nssienphilip <95418182+12philip34@users.noreply.github.com>
2023-03-16 18:31:40 +0100

    updated mail

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import style from "./footerStyle.module.css";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import EmailIcon from '@mui/icons-material/Email';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Footer = () => {
-
   const LinkIcon = {
-    color: 'white'
-  }
+    color: "white",
+  };
   return (
     <div>
       <div className={style.footer} id="ContactUs">
@@ -27,10 +26,22 @@ const Footer = () => {
             </p>
           </span>
           <span className={style.footerLinks} data-aos="zoom-in">
-            <a href="https://www.linkedin.com/company/bigjo-concierge-service/"><LinkedInIcon style={LinkIcon} /></a>
-            <a href="https://twitter.com/BigJoConcierge"><TwitterIcon style={LinkIcon} /></a>
-            <a href=""><EmailIcon style={LinkIcon} /></a>
-            <a href="https://wa.me/message/6EKOUHPFZHV3N1"><WhatsAppIcon style={LinkIcon} /></a>
+            <a href="https://www.linkedin.com/company/bigjo-concierge-service/" target="_blank">
+              <LinkedInIcon style={LinkIcon} />
+            </a>
+            <a href="https://twitter.com/BigJoConcierge" target="_blank">
+              <TwitterIcon style={LinkIcon} />
+            </a>
+            <a
+              href="https://bigjoconcierge.com/_link/?link=mailto%3Abigjo%40bigjoconcierge.com&target=uQCiQh9f3p8UWppAvxu4KWkeQtPbUScBBb3psFfgnIFTAuwgmEIHy3h0ToqzGP1GRTVQfGUT6ff7APPFzZP3bJtR%2FxsnOfH%2FAXcE76n%2F1VZYRaU%3D&iv=i7BkEls3pJ5b58Ui"
+              title="bigjo@bigjoconcierge.com"
+              target="_blank"
+            >
+              <EmailIcon style={LinkIcon} />
+            </a>
+            <a href="https://wa.me/message/6EKOUHPFZHV3N1" target="_blank">
+              <WhatsAppIcon style={LinkIcon} />
+            </a>
           </span>
         </section>
         <section className={style.footerCopywrite}>
